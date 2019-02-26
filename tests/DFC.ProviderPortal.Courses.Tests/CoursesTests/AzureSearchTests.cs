39a2b9ba1d135edac9ee68aa041e65fa4d6ba537
Ian Rose <ian.findacourse@gmail.com>
2019-02-26 11:21:12 +0000

    wip

---
+++ tests/DFC.ProviderPortal.Courses.Tests/CoursesTests/AzureSearchTests.cs
@@ -0,0 +1,120 @@
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Primitives;
+using Dfc.ProviderPortal.FindACourse.Helpers;
+using Dfc.ProviderPortal.FindACourse.Interfaces;
+using Dfc.ProviderPortal.FindACourse.Models;
+using Dfc.ProviderPortal.FindACourse.Services;
+using Dfc.ProviderPortal.FindACourse.Settings;
+using Dfc.ProviderPortal.FindACourse.Tests.Helpers;
+using Newtonsoft.Json;
+using Xunit;
+using Moq;
+
+
+namespace Dfc.ProviderPortal.Courses.Tests.CoursesTests
+{
+    public class AzureSearchTests
+    {
+        private const string URI_PATH = "http://localhost:7071/api/";
+
+        private IConfiguration _config;
+        private ICourseService _service;
+        private SearchCriteriaStructure _criteria;
+
+        public AzureSearchTests()
+        {
+            //TestHelper.AddEnvironmentVariables();
+
+            _config = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory)
+                                                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                                                .AddEnvironmentVariables()
+                                                .Build();
+
+            ServiceCollection services = new ServiceCollection();
+            services.Configure<CosmosDbCollectionSettings>(_config.GetSection(nameof(CosmosDbCollectionSettings)))
+                    .Configure<CosmosDbSettings>(_config.GetSection(nameof(CosmosDbSettings)))
+                    .Configure<ProviderServiceSettings>(_config.GetSection(nameof(ProviderServiceSettings)))
+                    .Configure<VenueServiceSettings>(_config.GetSection(nameof(VenueServiceSettings)))
+                    .Configure<QualificationServiceSettings>(_config.GetSection(nameof(QualificationServiceSettings)))
+                    .Configure<SearchServiceSettings>(_config.GetSection(nameof(SearchServiceSettings)))
+                    .AddScoped<ICosmosDbHelper, CosmosDbHelper>()
+                    .AddScoped<IProviderServiceWrapper, ProviderServiceWrapper>()
+                    .AddScoped<ICourseService, CoursesService>()
+                    .AddScoped<IVenueServiceWrapper, VenueServiceWrapper>()
+                    .AddScoped<ISearchServiceWrapper, SearchServiceWrapper>();
+            ServiceProvider provider = services.BuildServiceProvider();
+
+            // Get CourseService instance
+            _service = provider.GetService<ICourseService>();
+
+            _criteria = new SearchCriteriaStructure() {
+                SubjectKeyword = "biol",
+                TownOrPostcode = "B1 2JP",
+                QualificationLevels = new string[] { "1", "2", "3", "4", "5", "7", "E" },
+                AttendanceModes = new string[] { },
+                AttendancePatterns = new string[] { "1", "2", "3" },
+                DFE1619Funded = "",
+                StudyModes = new string[] { },
+                Distance = 200,
+                TopResults = 100
+            };
+        }
+
+
+        //[Fact]
+        //public void RunTests()
+        //{
+        //    _SearchCourse_Run();
+        //    _CourseDetail_Run();
+        //    Assert.True(true);
+        //}
+
+        [Fact]
+        public async void _SearchCourse_Run()
+        {
+            Task<FACSearchResult> task = _service.CourseSearch(NullLoggerFactory.Instance.CreateLogger("Null Logger"), _criteria);
+            FACSearchResult result = await task;
+            Assert.True(result != null && result.Value?.Count() > 0);
+        }
+
+
+        [Fact]
+        public async void _CourseDetail_Run()
+        {
+            Task<FACSearchResult> task = _service.CourseSearch(NullLoggerFactory.Instance.CreateLogger("Null Logger"), _criteria);
+            FACSearchResult result = await task;
+
+            DefaultHttpRequest request = new DefaultHttpRequest(new DefaultHttpContext()) {
+                Query = new QueryCollection(new Dictionary<string, StringValues>
+                {
+                    { "CourseId", result.Value.Last().CourseId.Value.ToString() },
+                    { "RunId", result.Value.Last().id.Value.ToString() }
+                })
+            };
+            IActionResult response = await CourseDetail.Run(
+                request,
+                NullLoggerFactory.Instance.CreateLogger("Null Logger"),
+                _service
+            );
+            AzureSearchCourseDetail detail = (AzureSearchCourseDetail)((OkObjectResult)response).Value;
+
+            Assert.True(detail != null && detail?.Course != null && detail?.Provider != null && detail?.Qualification != null);
+        }
+
+    }
+}
---
+++ tests/DFC.ProviderPortal.Courses.Tests/Helpers/TestHelper.cs
@@ -0,0 +1,95 @@
+﻿
+using System;
+using System.IO;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Moq;
+
+
+namespace Dfc.ProviderPortal.FindACourse.Tests.Helpers
+{
+    public static class TestHelper
+    {
+        public static void AddEnvironmentVariables()
+        {
+            // Add environment variables needed to test Azure Functions here (launchSettings.json doesn't get processed by test projects)
+            Environment.SetEnvironmentVariable("APPSETTING_EndpointUri", "https://something.azure.com/");
+            Environment.SetEnvironmentVariable("APPSETTING_PrimaryKey", "****************************==");
+            Environment.SetEnvironmentVariable("APPSETTING_DatabaseId", "*******");
+            //Environment.SetEnvironmentVariable("APPSETTING_Collection", "********");
+        }
+
+        /// <summary>
+        /// Read the result content stream from a Azure Function and return as IEnumerable<Model>
+        /// </summary>
+        /// <typeparam name="T">Model type</typeparam>
+        /// <param name="task">Task associated with async calling of Azure Function</param>
+        /// <returns>Content deserialized into IEnumerable<T></returns>
+        public static IEnumerable<T> GetAFReturnedObjects<T>(Task<HttpResponseMessage> task)
+        {
+            // Run the Azure Function to get the data, then get the returned StringContent holding returned data as JSON
+            task.Wait();
+            StringContent sc = (StringContent)task.Result.Content;
+
+            // Read the content stream
+            Task<string> task2 = sc.ReadAsStringAsync();
+            task2.Wait();
+
+            // Deserialize in an IEnumerable<T> to return
+            return JsonConvert.DeserializeObject<IEnumerable<T>>(task2.Result);
+        }
+
+        /// <summary>
+        /// Read the result content stream from a Azure Function and return as IEnumerable<Model>
+        /// </summary>
+        /// <typeparam name="T">Model type</typeparam>
+        /// <param name="task">Task associated with async calling of Azure Function</param>
+        /// <returns>Content deserialized into IEnumerable<T></returns>
+        public static T GetAFReturnedObject<T>(Task<HttpResponseMessage> task)
+        {
+            // Run the Azure Function to get the data, then get the returned StringContent holding returned data as JSON
+            task.Wait();
+            StringContent sc = (StringContent)task.Result.Content;
+
+            // Read the content stream
+            Task<string> task2 = sc.ReadAsStringAsync();
+            task2.Wait();
+
+            // Deserialize in an IEnumerable<T> to return
+            return JsonConvert.DeserializeObject<T>(task2.Result);
+        }
+
+        public static HttpRequestMessage CreateRequest(Uri uri, string json)
+        {
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = uri,
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+            request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+            return request;
+        }
+
+        public static Mock<HttpRequest> CreateMockRequest(object body)
+        {
+            MemoryStream ms = new MemoryStream();
+            StreamWriter sw = new StreamWriter(ms);
+
+            string json = JsonConvert.SerializeObject(body);
+            sw.Write(json);
+            sw.Flush();
+            ms.Position = 0;
+
+            Mock<HttpRequest> request = new Mock<HttpRequest>();
+            request.Setup(x => x.Body).Returns(ms);
+            return request;
+        }
+    }
+}
