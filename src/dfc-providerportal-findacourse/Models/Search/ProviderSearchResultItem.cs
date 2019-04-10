de245f87631560fe7daee4dc622a9553b70a0a50
Ian Rose <ian.findacourse@gmail.com>
2019-04-10 11:36:57 +0100

    DFC-8393 wip

--- src/dfc-providerportal-findacourse/Models/Search/ProviderSearchResultItem.cs
+++ src/dfc-providerportal-findacourse/Models/Search/ProviderSearchResultItem.cs
@@ -1,20 +1,15 @@
 ﻿
 using System;
-//using System.Linq;
-//using System.Collections.Generic;
 
 
 namespace Dfc.ProviderPortal.FindACourse.Models
 {
-    //[Serializable]
     public class ProviderSearchResultItem //: AzureSearchCourse
     {
         public Guid id { get; set; }
         public string Name { get; set; }
         public string Postcode { get; set; }
-        //[NonSerialized]
-        //public IEnumerable<string> Town { get; set; } //TownArray; // { get; set; }
-        public string Town { get; set; } // { return TownArray.FirstOrDefault(); } }
+        public string Town { get; set; }
         public string Region { get; set; }
         public string ProviderId { get; set; }
     }
