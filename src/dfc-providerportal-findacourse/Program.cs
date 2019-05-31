1051b9d49481ffcd060711d167799ae98d7a7653
Ian Rose <ian.findacourse@gmail.com>
2019-05-31 09:08:41 +0100

    Added support for App Insights

--- src/dfc-providerportal-findacourse/Program.cs
+++ src/dfc-providerportal-findacourse/Program.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
@@ -8,17 +7,23 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+
 namespace Dfc.ProviderPortal.FindACourse.API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build()
+                                      .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                   .UseStartup<Startup>()
+                   .ConfigureLogging(builder =>
+                   {
+                       builder.AddApplicationInsights("2bdf58ed-77ed-4b5c-8c7a-eea63685945a");
+                   });
     }
 }
