2a40db6c89b02034801a5eeb07460e2b7e92643d
Ian Rose <ian.findacourse@gmail.com>
2019-09-26 15:38:52 +0100

    COUR-2014 CourseGet API

---
+++ src/dfc-providerportal-findacourse/Models/Search/CourseDetailStructure.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+
+namespace Dfc.ProviderPortal.FindACourse.Models
+{
+    public class CourseDetailStructure
+    {
+        public Guid CourseId { get; set; }
+        public Guid RunId { get; set; }
+    }
+}
