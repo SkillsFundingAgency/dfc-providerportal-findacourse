86686b4bff2ea7a922d45696225408fd5cd424b8
Ian Rose <ian.findacourse@gmail.com>
2019-09-30 15:46:37 +0100

    COUR-1912 Added missing fields

--- src/dfc-providerportal-findacourse/Interfaces/IAzureSearchCourse.cs
+++ src/dfc-providerportal-findacourse/Interfaces/IAzureSearchCourse.cs
@@ -21,8 +21,20 @@ namespace Dfc.ProviderPortal.FindACourse.Interfaces
         string VenueAddress { get; set; }
         GeographyPoint VenueLocation { get; set; }
         string VenueAttendancePattern { get; set; }
+        string VenueAttendancePatternDescription { get; set; }
         string ProviderName { get; set; }
         string Region { get; set; }
+        decimal ScoreBoost { get; set; }
         int? Status { get; set; }
+        string VenueStudyMode { get; set; }
+        string VenueStudyModeDescription { get; set; }
+        string DeliveryMode { get; set; }
+        string DeliveryModeDescription { get; set; }
+        DateTime? StartDate { get; set; }
+        string VenueTown { get; set; }
+        int? Cost { get; set; }
+        string CostDescription { get; set; }
+        string CourseText { get; set; }
+        string UKPRN { get; set; }
     }
 }
