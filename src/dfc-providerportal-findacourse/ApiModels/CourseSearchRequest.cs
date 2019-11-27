e84a1b940137cc71d0f9bd7a8a63615229485f49
James Gunn <james@gunn.io>
2019-11-27 09:27:49 +0000

    Add Request models for API endpoints

---
+++ src/dfc-providerportal-findacourse/ApiModels/CourseSearchRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Dfc.ProviderPortal.FindACourse.Models;
+
+namespace Dfc.ProviderPortal.FindACourse.ApiModels
+{
+    public class CourseSearchRequest
+    {
+        public string SubjectKeyword { get; set; }
+        public string DFE1619Funded { get; set; }
+        public float? Distance { get; set; }
+        public int[] QualificationLevels { get; set; }
+        public int[] StudyModes { get; set; }
+        public int[] AttendanceModes { get; set; }
+        public int[] AttendancePatterns { get; set; }
+        public string Town { get; set; }
+        public string Postcode { get; set; }
+        public CourseSearchSortBy? SortBy { get; set; }
+        public DateTime? StartDateFrom { get; set; }
+        public DateTime? StartDateTo { get; set; }
+
+        public int? TopResults { get; set; }
+        public int? PageNo { get; set; }
+    }
+}
