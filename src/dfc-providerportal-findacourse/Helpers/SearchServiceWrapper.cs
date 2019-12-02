d0b1f5828a1c62d1377a7a024414fd868dc19a51
James Gunn <james@gunn.io>
2019-12-02 15:07:35 +0000

    Add a few more facets

--- src/dfc-providerportal-findacourse/Helpers/SearchServiceWrapper.cs
+++ src/dfc-providerportal-findacourse/Helpers/SearchServiceWrapper.cs
@@ -199,7 +199,15 @@ namespace Dfc.ProviderPortal.FindACourse.Helpers
                 $"{criteria.SubjectKeyword}*",
                 new SearchParameters()
                 {
-                    Facets = new[] { "NotionalNVQLevelv2", "VenueAttendancePattern", "ProviderName", "Region" },
+                    Facets = new[]
+                    {
+                        "NotionalNVQLevelv2",
+                        "VenueStudyMode",
+                        "VenueAttendancePattern",
+                        "DeliveryMode",
+                        "ProviderName",
+                        "Region"
+                    },
                     Filter = filter,
                     IncludeTotalResultCount = true,
                     ScoringProfile = scoringProfile,
